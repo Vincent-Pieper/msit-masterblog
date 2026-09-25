09b4db2adaeb5d6ec8a619b50f7ebefd9cd6c689
Vincent Pieper <pieper.vincent@gmail.com>
2026-09-25 18:10:01 +0200

    Add blog post creation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import storage
 
 
@@ -12,6 +12,24 @@ app = Flask(__name__)
 def index():
     blog_posts = storage.get_json(DATA)
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        new_input = request.form.to_dict()
+        blog_posts = storage.get_json(DATA)
+        new_id = storage.get_available_id(blog_posts)
+        new_blogpost = storage.create_new_blogpost(new_input, new_id)
+        blog_posts.append(new_blogpost)
+
+        if new_id != len(blog_posts):
+            storage.reorder_blogposts_by_id(blog_posts)
+        storage.save_json(DATA, blog_posts)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
--- storage.py
+++ storage.py
@@ -5,3 +5,41 @@ def get_json(filename: str) -> list[dict]:
     with open(filename, "r") as json_read:
         return json.load(json_read)
 
+
+def save_json(filename: str, file: list[dict]):
+    with open(filename, "w") as json_write:
+        json_write.write(json.dumps(file, indent=4))
+
+
+def get_available_id(blogposts: list[dict]):
+    used_ids = set([blogpost["id"] for blogpost in blogposts])
+    ordered_ids = sorted(used_ids)
+
+    return find_smallest_id(ordered_ids)
+
+
+def find_smallest_id(ordered_ids: list[int]) -> int:
+    expected_id = 1
+    for current_id in ordered_ids:
+        if expected_id != current_id:
+            return expected_id
+        expected_id += 1
+
+    return expected_id
+
+
+def create_new_blogpost(new_post: dict, new_id: int) -> dict:
+    return {
+        "id": new_id,
+        "author": new_post["author"],
+        "title": new_post["title"],
+        "content": new_post["content"]
+    }
+
+def reorder_blogposts_by_id(blog_posts: list[dict]) -> None:
+    blog_posts.sort(key=lambda x: x["id"])
+
+
+
+
+
